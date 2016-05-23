5311ff4f33fa74c172fccc23438e2d12997960a4
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-05-23 15:41:35 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='timbr-machine',
+      version='0.1',
+      description='Dask-based data collection and processing machine',
+      url='https://bitbucket.com/timbr-io/timbr-machine',
+      author='Pramukta Kumar',
+      author_email='pramukta.kumar@timbr.io',
+      license='MIT',
+      packages=['timbr'],
+      zip_safe=False,
+      install_requires=[
+          "pymongo>=2.8",
+          "dask",
+          "twisted",
+          "ipython"
+        ]
+      )
