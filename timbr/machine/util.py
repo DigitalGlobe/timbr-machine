4dc1a384e2870d85a358cc13f2725c0c2d9e3a9f
Pramukta Kumar <pramukta.kumar@gmail.com>
2016-05-24 11:22:51 -0500

    util module to hold general things that don't belong elsewhere

---
+++ timbr/machine/util.py
@@ -0,0 +1,18 @@
+import threading
+
+
+# Stolen from StackOverflow:
+# http://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread-in-python
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self):
+        super(StoppableThread, self).__init__()
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
