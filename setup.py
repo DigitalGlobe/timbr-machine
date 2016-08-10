3b8259de57f54d211ee1b19c3fd8113613615bf0
Chris Helm <christopher.helm@gmail.com>
2016-08-10 17:45:18 +0200

    fixing the setup file to support non notebook based installs (no jupyter)

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
 from setuptools import setup
 from setuptools.command.develop import develop as _develop
 from setuptools.command.install import install as _install
-from notebook.nbextensions import install_nbextension
-from notebook.services.config import ConfigManager
 import os
 
 extension_dir = os.path.join(os.path.dirname(__file__), "timbr", "static")
 
 class develop(_develop):
-    def run(self):
-        _develop.run(self)
-        install_nbextension(extension_dir, symlink=True,
-                            overwrite=True, user=True, destination="timbr_machine")
-        cm = ConfigManager()
-        cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
+    try:
+        from notebook.nbextensions import install_nbextension
+        from notebook.services.config import ConfigManager
+        def run(self):
+            _develop.run(self)
+            install_nbextension(extension_dir, symlink=True,
+                                overwrite=True, user=True, destination="timbr_machine")
+            cm = ConfigManager()
+            cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
+    except:
+        pass
 
 class install(_install):
-    def run(self):
-        _install.run(self)
-        cm = ConfigManager()
-        cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
+    try:
+        from notebook.nbextensions import install_nbextension
+        from notebook.services.config import ConfigManager
+        def run(self):
+            _install.run(self)
+            cm = ConfigManager()
+            cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
+    except:
+        pass
 
 setup(name='timbr-machine',
       cmdclass={'develop': develop, 'install': install},
@@ -40,8 +48,8 @@ setup(name='timbr-machine',
           "pymongo>=2.8",
           "dask",
           "ipython",
-          "jupyter-react",
-          "observed"
+          "observed",
+          "jupyter_react"
         ],
       tests_require=[
           "nose",
