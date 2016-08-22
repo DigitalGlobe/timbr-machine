04461e2f7634725ce47dd3ed85d4d876a1ad9af7
Jamie Polackwich <rjpolackwich@gmail.com>
2016-08-22 17:15:03 -0500

    put stuff in a twola compat class

--- timbr/compat/twola_compat.py
+++ timbr/compat/twola_compat.py
@@ -1,61 +1,68 @@
 from __future__ import print_function
-from timbr.machine import Machine 
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 import yaml
 import os
 import shlex
-import optparse
-import imp
 
-def _compile_forest(x, stk=[]):
-    nodes = []
-    if x is None:
-        return nodes
-    elif isinstance(x, list):
-        for item in x:
-            nodes.extend(_compile_forest(item))
-    elif isinstance(x, dict):
-        for k in x:
-            nodes.extend(_compile_forest(k))
-            stk.append(k)
-            nodes.extend(_compile_forest(x[k], stk))
-            stk.pop()
-    else:
-        stk.append(x)
-        nodes.append(tuple(stk))
-        stk.pop()
-    return nodes
 
-def generate_template(ntransforms):
-    lines = []
-    lines.append("from timbr.machine import Machine\nMACHINE=Machine()\nfrom {source[0]} import {source[1]}")
-    lines.append("MACHINE.set_source({source[1]}())")
-    for i in range(ntransforms):
-        lines.append("from {f" + str(i) + "[0]} import {f" + str(i) + "[1]}")
-        lines.append("MACHINE[" + str(i) + "] = {f" + str(i) + "[1]}")
-    lines.append("MACHINE.start()")
-    return "\n".join(lines)
+class TwolaCompatibilityStructure(object):
+    def __init__(self, project_path):
+        self.project_path = project_path
+        with open(os.path.join(project_path, "topology.yaml")) as f:
+            tree = yaml.load(f)
+        self._data = _compile_forest(tree)[-1]
+        self.twola = self._twola()        
+        self.init_template = self._init_template()
 
-def get_module_entrypoint(full_mod_path):
-    assert(os.path.exists(full_mod_path))
-    with open(full_mod_path) as f:
-        f.readline()
-        cmds = f.readline()
-    args = shlex.split(cmds)
-    return args[3].strip("()")
-    
-def generate_project_structure(project_path):
-    ds = OrderedDict()
-    with open(os.path.join(project_path, "topology.yaml")) as f:
-        tree = yaml.load(f)
-    pdata = _compile_forest(tree)[-1]
-    # source data
-    modfile, _, modname = shlex.split(pdata[0])
-    entrypoint = get_module_entrypoint(modfile)
-    ds["source"] = (modname, entrypoint)
-    # transform data
-    for ind, cmd in enumerate(pdata[1:]):
-        modfile, _, modname = shlex.split(cmd)
-        entrypoint = get_module_entrypoint(os.path.join(project_path, modfile))
-        ds["f{}".format(ind)] = (modname, entrypoint)
-    return ds
+        @classmethod
+        def _compile_forest(x, stk=[]):
+            nodes = []
+            if x is None:
+                return nodes
+            elif isinstance(x, list):
+                for item in x:
+                    nodes.extend(_compile_forest(item))
+            elif isinstance(x, dict):
+                for k in x:
+                    nodes.extend(_compile_forest(k))
+                    stk.append(k)
+                    nodes.extend(_compile_forest(x[k], stk))
+                    stk.pop()
+            else:
+                stk.append(x)
+                nodes.append(tuple(stk))
+                stk.pop()
+            return nodes
+
+        @classmethod
+        def _get_module_entrypoint(modfile):
+            assert(os.path.exists(full_mod_path))
+            with open(full_mod_path) as f:
+                f.readline()
+                cmds = f.readline()
+            args = shlex.split(cmds)
+            return args[3].strip("()")
+
+        def _twola(self):
+            ds = []
+            for cmd in self._data:
+                modfile, _, modname = shlex.split(cmd)
+                entrypoint = get_module_entrypoint(modfile)
+                ds.append(machine_compatible_effect._make(module, entrypoint, name))
+            return ds
+
+        def _init_template(self):
+            lines = ["from {[" + str(i) + "].module} import {[" + str(i) + "].entrypoint}" for i in range(len(self.twola))]
+            return "\n".join(lines)
+
+        def machine_init(self):
+            s = self.init_template
+            return s.format(*self.twola)
+     
+        def machine_config(self):
+            s = {}
+            s["source"] = self.twola["source"]._asdict()
+            s["transforms"] = {}
+            for ind, fn in enumerate(self.twola[1:]):
+                s["transforms"]["f{}".format(ind)] = fn._asdict()
+            return s
