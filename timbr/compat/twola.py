48bb49692335dc3d808018f15a4aa17d55cbcea9
Jamie Polackwich <rjpolackwich@gmail.com>
2016-08-24 17:47:48 -0500

    refactored class

--- timbr/compat/twola.py
+++ timbr/compat/twola.py
@@ -4,78 +4,77 @@ import yaml
 import os
 import shlex
 import json
+import shutil
 
 
-machine_effect = namedtuple("MachineEffect", ["module", "effect", "name"])
+twola_module = namedtuple("TwolaModule", ["modfile", "modname", "effect"])
 
-class TwolaProjectStore(object):
-    def __init__(self, project_path):
-        self.project_path = project_path
-        with open(os.path.join(project_path, "topology.yaml")) as f:
-            tree = yaml.load(f)
-        self._data = self._compile_forest(tree)[-1]
-        self._twola = self._twola_store()        
-
-    def _compile_forest(self, x, stk=[]):
-        nodes = []
-        if x is None:
-            return nodes
-        elif isinstance(x, list):
-            for item in x:
-                nodes.extend(self._compile_forest(item))
-        elif isinstance(x, dict):
-            for k in x:
-                nodes.extend(self._compile_forest(k))
-                stk.append(k)
-                nodes.extend(self._compile_forest(x[k], stk))
-                stk.pop()
-        else:
-            stk.append(x)
-            nodes.append(tuple(stk))
-            stk.pop()
+def _compile_forest(x, stk=[]):
+    nodes = []
+    if x is None:
         return nodes
+    elif isinstance(x, list):
+        for item in x:
+            nodes.extend(_compile_forest(item))
+    elif isinstance(x, dict):
+        for k in x:
+            nodes.extend(_compile_forest(k))
+            stk.append(k)
+            nodes.extend(_compile_forest(x[k], stk))
+            stk.pop()
+    else:
+        stk.append(x)
+        nodes.append(tuple(stk))
+        stk.pop()
+    return nodes
 
-    @staticmethod
-    def _get_module_effect(modfile):
-        assert(os.path.exists(modfile))
-        with open(modfile) as f:
-            f.readline()
-            cmds = f.readline()
-        args = shlex.split(cmds)
-        return args[3].strip("()")
+def _get_module_effect(modfile):
+    assert(os.path.exists(modfile))
+    with open(modfile) as f:
+        f.readline()
+        cmds = f.readline()
+    args = shlex.split(cmds)
+    return args[3].strip("()")
 
-    @property
-    def init_template(self):
-        lines = ["from {0.module} import {0.effect} as _source"]
-        lines.extend(["from {" + str(i + 1) + ".module} import {" + str(i + 1) + ".effect} as _f" + str(i) for i in range(len(self._twola) - 1)])
-        return "\n".join(lines)
+def init_template(nfuncs):
+    lines = ["from {0.modname} import {0.effect} as _source"]
+    lines.extend(["from {" + str(i + 1) + ".modname} import {" + str(i + 1) + ".effect} as _f" + str(i) for i in range(nfuncs)])
+    return "\n".join(lines)
 
-    def _twola_store(self):
-        ds = []
-        for cmd in self._data:
-            modfile, _, modname = shlex.split(cmd)
-            effect = self._get_module_effect(modfile)
-            ds.append(machine_effect(modname, effect, modname))
-        return ds
+def topology_to_data(yaml_path):
+    with open(path) as f:
+        tree = yaml_path.load(f)
+    _data = _compile_forest(tree)[-1]
+    ds = []
+    for cmd in _data:
+        modfile, _, modname = shlex.split(cmd)
+        effect = _get_module_effect(modfile)
+        ds.append(machine_effect(modfile, modname, effect))
+    return ds
 
-    def machine_init(self):
-        s = self.init_template
-        return s.format(*self._twola)
- 
-    def machine_config(self):
-        s = {"_module_path": self.project_path}
-        s["source"] = self._twola[0]._asdict()
-        s["transforms"] = []
-        for fn in self._twola[1:]:
-            s["transforms"].append(fn._asdict())
-        return s
+def machine_config(init_path, twola):
+    ds = {"machine_init_path": init_path}
+    ds["source"] = twola[0]._asdict()
+    ds["transforms"] = []
+    for fn in twola[1:]:
+        ds["transforms"].append(fn._asdict())
+    return ds
 
-def configure_twola_project(project_path=None):
-    if project_path is None:
-        project_path = os.getcwd()
-    tps = TwolaProjectStore(project_path)
-    with open(os.path.join(project_path, "_machine_.py"), "w") as f:
-        f.write(tps.machine_init())
-    with open(os.path.join(project_path, "machine.json"), "w") as f:
-        json.dump(tps.machine_config(), f)
-
+def convert_twola_project(project_path, target_path=None):
+    assert os.path.isdir(project_path)
+    twola = yaml_to_data(os.path.join(project_path, "topology.yaml"))
+    if target_path is None:
+        target_path = project_path
+    else:
+        for mod in twola:
+            shutil.copy(os.path.join(project_path, mod.modfile), target_path)    
+    # copy files 
+    #write __init__.py
+    init = init_template(len(twola) - 1)
+    formatted = init.format(*twola)
+    with open(os.path.join(target_path, "__init__.py"), "w") as f:
+        f.write(formatted)
+    # write machine datastructure
+    ds = machine_config(os.path.join(target_path, "__init__.py"), twola)
+    with open(os.path.join(target_path, "machine.json"), "w") as f:
+        json.dump(ds)
