27f78a5513d1348d9d0f9230b373adc5d6ef4a36
Jamie Polackwich <rjpolackwich@gmail.com>
2016-09-07 12:16:18 -0500

    merging new setup file

--- setup.py
+++ setup.py
@@ -3,25 +3,29 @@ from setuptools.command.develop import develop as _develop
 from setuptools.command.install import install as _install
 import os
 
+try: 
+    from notebook.nbextensions import install_nbextension
+    from notebook.services.config import ConfigManager
+except ImportError:
+    install_nbextension = None
+    ConfigManager = None
+
 extension_dir = os.path.join(os.path.dirname(__file__), "timbr", "static")
 
 class develop(_develop):
     try:
-        from notebook.nbextensions import install_nbextension
-        from notebook.services.config import ConfigManager
         def run(self):
-            _develop.run(self)
-            install_nbextension(extension_dir, symlink=True,
+            if install_nbextension is not None and ConfigManager is not None:
+                _develop.run(self)
+                install_nbextension(extension_dir, symlink=True,
                                 overwrite=True, user=True, destination="timbr_machine")
-            cm = ConfigManager()
-            cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
+                cm = ConfigManager()
+                cm.update('notebook', {"load_extensions": {"timbr_machine/index": True } })
     except:
         pass
 
 class install(_install):
     try:
-        from notebook.nbextensions import install_nbextension
-        from notebook.services.config import ConfigManager
         def run(self):
             _install.run(self)
             cm = ConfigManager()
@@ -39,11 +43,6 @@ setup(name='timbr-machine',
       license='MIT',
       packages=['timbr', 'timbr.machine'],
       zip_safe=False,
-      entry_points = {
-        'console_scripts': [
-            "machine-captd = twola.capture:main",
-            ]
-        },
       data_files=[
         ('share/jupyter/nbextensions/timbr_machine', [
             'timbr/static/index.js'
@@ -54,8 +53,7 @@ setup(name='timbr-machine',
           "dask",
           "ipython",
           "observed",
-          "jupyter_react",
-          "subprocess32"
+          "jupyter_react"
         ],
       tests_require=[
           "nose",
