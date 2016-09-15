7fd5672b514e5aa4dc353bc3c03dfc7836c50816
glortho <jed@dataverse.me>
2016-09-14 20:04:02 -0400

    getting update to work

--- timbr/machine/dashboard.py
+++ timbr/machine/dashboard.py
@@ -3,7 +3,7 @@ from jupyter_react import Component
 from time import sleep
 
 class Dashboard(Component):
-    """ 
+    """
       A UI component for displaying status info in notebooks via react components
     """
     def __init__(self, machine, interval=1, module=None, **kwargs):
@@ -11,10 +11,13 @@ class Dashboard(Component):
         self.on_msg(self._handle_msg)
         self.machine = machine
         self._module = module
-        self.send({"method": "display", "props": {"config": self.machine._config}})
+        self.send({"method": "display", "props": {"config": {"foo": "bar"}}})
 
     def _handle_msg(self, msg):
         data = msg['content']['data']
         if data.get('method', None) == 'capture' and data['data']['action'] in ['start', 'stop']:
             method = getattr(self.machine, data['data']['action'])
             method()
+
+    def update(self):
+        self.send({'method': 'update', 'props': { 'status': 'foo' }})
