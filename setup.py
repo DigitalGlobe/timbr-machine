35768a949ceaff15621b8b5a265eb1a5964c9585
Chris Helm <christopher.helm@gmail.com>
2017-01-16 12:15:16 -0700

    recursive include

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='timbr-machine',
       data_files=[
         ('share/jupyter/nbextensions/timbr_machine', [
             'timbr/static/index.js'
-        ]),
+        ])
       ],
       install_requires=[
           "pymongo>=2.8",
